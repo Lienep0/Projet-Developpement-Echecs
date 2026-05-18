7568776c916cfe159b97889d234b3754460d342f
Louka <loukasiegel@gmail.com>
2026-05-18 14:24:37 +0200

    encoding fini

--- IA_Louka/data/encoding.py
+++ IA_Louka/data/encoding.py
@@ -11,19 +11,19 @@ def update_value_head(game, board):
     result = game.headers.get("Result")
     #victory
     if result == "1-0":
-        value[2] = 1.0 
+        value[0] = 1.0 
     #defeat
     elif result == "0-1":
-        value[0] = 1.0   
+        value[2] = 1.0   
     #draw
     else:
         value[1] = 1.0
     #current player
     if board.turn == chess.BLACK:
-        value = value[::-1]
+        value = value[::-1].copy()
     return value 
 
-def encode_pieces(board, entry):
+def encode_pieces(board, entry, flip):
     
     piece_types = [
         chess.PAWN, 
@@ -37,18 +37,25 @@ def encode_pieces(board, entry):
     for i, j in enumerate(piece_types):
         for square in board.pieces(j, chess.WHITE):
             row, col = divmod(square, 8)
+            if flip: 
+                row = 7-row
+                col = 7-col
             entry[i, row, col] = 1.0
-
         for square in board.pieces(j, chess.BLACK):
             row, col = divmod(square, 8)
+            if flip: 
+                row = 7-row
+                col = 7-col
             entry[i + 6, row, col] = 1.0
-    return entry
 
 
 
-def fill_tactical_analysis(value, board):
+def fill_tactical_analysis(value, board, flip):
     for square in chess.SQUARES:
         row, col = divmod(square, 8)
+        if flip: 
+            row = 7-row
+            col = 7-col
         if board.is_attacked_by(board.turn, square):
             value[21, row, col] = 1.0
         if board.is_attacked_by(not board.turn, square):
@@ -58,6 +65,9 @@ def fill_tactical_analysis(value, board):
         piece = board.piece_at(square)
         if piece:
             row, col = divmod(square, 8)
+            if flip: 
+                row = 7-row
+                col = 7-col
             if board.is_pinned(piece.color, square):
                 value[23, row, col] = 1.0
                 
@@ -65,22 +75,32 @@ def fill_tactical_analysis(value, board):
 
 def update_entry(board):
     value = np.zeros((24, 8, 8), dtype=np.float32)
-    encode_pieces(board,value)
+    flip = True if board.turn == chess.BLACK else False
+    encode_pieces(board, value, flip)
     value[12] = np.ones((8, 8))
     max_moves = 100.0
     current_move = board.fullmove_number
     normalized_value = min(current_move / max_moves, 1.0)
     value[13] = normalized_value
-    value[14] = float(board.has_kingside_castling_rights(chess.WHITE))
-    value[15] = float(board.has_queenside_castling_rights(chess.WHITE))
-    value[16] = float(board.has_kingside_castling_rights(chess.BLACK))
-    value[17] = float(board.has_queenside_castling_rights(chess.BLACK))
+    if flip:
+        value[14] = float(board.has_kingside_castling_rights(chess.BLACK))
+        value[15] = float(board.has_queenside_castling_rights(chess.BLACK))
+        value[16] = float(board.has_kingside_castling_rights(chess.WHITE))
+        value[17] = float(board.has_queenside_castling_rights(chess.WHITE))
+    else:
+        value[14] = float(board.has_kingside_castling_rights(chess.WHITE))
+        value[15] = float(board.has_queenside_castling_rights(chess.WHITE))
+        value[16] = float(board.has_kingside_castling_rights(chess.BLACK))
+        value[17] = float(board.has_queenside_castling_rights(chess.BLACK))
     if board.ep_square:
         row, col = divmod(board.ep_square, 8)
+        if flip: 
+            row = 7-row
+            col = 7-col
         value[18, row, col] = 1.0
     value[19] = min(board.halfmove_clock / 100.0, 1.0)
     value[20] = 1.0 if board.is_check() else 0.0
-    value = fill_tactical_analysis(value, board)
+    value = fill_tactical_analysis(value, board, flip)
     return value
 
 def update_policy_head(move, board):
@@ -96,16 +116,28 @@ def update_policy_head(move, board):
     diff_col = to_col - from_col
     distance = max(abs(diff_row), abs(diff_col))
     if board.turn == chess.BLACK:
-        from_row, from_col = 7 - from_row, from_col
-        to_row, to_col = 7 - to_row, from_col
-        diff_row = -diff_row
+        from_row = 7 - from_row
+        from_col = 7 - from_col
 
-    if move.promotion and move.promotion != chess.QUEEN:
-        direction = diff_col + 1 
-        piece_type = {chess.KNIGHT: 0, chess.BISHOP: 1, chess.ROOK: 2}[move.promotion]
-        plan_idx = 64 + (3 * piece_type) + direction
-        policy[plan_idx, from_row, from_col] = 1.0
-        return policy
+        to_row = 7 - to_row
+        to_col = 7 - to_col
+
+        diff_row = to_row - from_row
+        diff_col = to_col - from_col
+
+
+
+    if move.promotion :
+            if move.promotion == chess.QUEEN:
+                plan_idx = 0 
+                policy[plan_idx, from_row, from_col] = 1.0
+                return policy
+            else:
+                direction = diff_col + 1 
+                piece_type = {chess.KNIGHT: 0, chess.BISHOP: 1, chess.ROOK: 2}[move.promotion]
+                plan_idx = 64 + (3 * piece_type) + direction
+                policy[plan_idx, from_row, from_col] = 1.0
+                return policy
 
     knight_moves = [
         (2, 1), (1, 2), (-1, 2), (-2, 1),
@@ -138,7 +170,6 @@ def create_hdf5_file(h5_path):
 
 
 def write_encode(h5_file,PgnFile,index):
-    buffer = 0
     while True:
         game = chess.pgn.read_game(PgnFile)
         if game is None:
@@ -146,6 +177,7 @@ def write_encode(h5_file,PgnFile,index):
         
         board = game.board() 
 
+        buffer = 0
         entries = []
         policy_heads = []
         value_heads = []
@@ -159,7 +191,7 @@ def write_encode(h5_file,PgnFile,index):
             entries.append(entry)
             policy_heads.append(policy_head)
             value_heads.append(value_head)
-            if buffer > 1000:
+            if buffer >= 1000:
                 index=save_position(h5_file, index,entries,policy_heads,value_heads,buffer)
                 buffer = 0
                 entries = []
@@ -198,15 +230,15 @@ def save_position(h5_file, index, entries,policy_heads,value_heads,buffer):
 def iterate(h5_file,directory_path, limit):
     files = os.listdir(directory_path)
     index=0
-    l=1
+    k=1
     for file in files:
         if file.endswith(".pgn"): 
             pgn = os.path.join(directory_path, file)
             with open(pgn, 'r') as f :
                 index=write_encode(h5_file,f,index)
-                if index>l*10000:
+                if index>k*10000:
                     print(index)
-                    l+=1
+                    k+=1
                 if index>limit:
                     break
         else:
