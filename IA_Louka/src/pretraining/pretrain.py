7880cf15ebd29e9c859d1da5917fee8c8b798cd2
Louka <loukasiegel@gmail.com>
2026-05-25 13:31:07 +0200

    entrainement plus fin et ameliore

--- IA_Louka/src/pretraining/pretrain.py
+++ IA_Louka/src/pretraining/pretrain.py
@@ -54,5 +54,5 @@ if __name__ == "__main__":
         value_heads = torch.from_numpy(f["value_head"][:]).float()
 
     dataset = TensorDataset(entries,policy_heads,value_heads)
-    dataloaded = DataLoader(dataset, batch_size=512, shuffle=True)
-    train(dataloaded,device,AI_path,20)
+    dataloaded = DataLoader(dataset, batch_size=1024, shuffle=True)
+    train(dataloaded,device,AI_path,10)
