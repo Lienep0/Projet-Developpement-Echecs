3ac28ab77a41a2f5134bc601cec846807f20533c
Charles Guo <charlesguo91@gmail.com>
2026-06-11 18:22:11 +0200

    zazeazedza

--- Shaarles2/Shaarles2/setup.py
+++ Shaarles2/Shaarles2/setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup
 from setuptools.command.build_ext import build_ext
+from distutils.sysconfig import get_config_vars
 from pybind11.setup_helpers import Pybind11Extension
 import sys
-import setuptools
+import os
+
+# Remove -Wstrict-prototypes (Python bug)
+(opt,) = get_config_vars("OPT")
+os.environ["OPT"] = " ".join(flag for flag in opt.split() if flag != "-Wstrict-prototypes")
 
 class BuildExt(build_ext):
     def build_extensions(self):
-        ct = self.compiler.compiler_type
+        # Force NVCC for .cu files
         for ext in self.extensions:
-            if hasattr(ext, "extra_compile_args"):
-                if ct == "unix":
-                    ext.extra_compile_args = ext.extra_compile_args.get("cxx", [])
-                else:
-                    ext.extra_compile_args = []
+            for i, src in enumerate(ext.sources):
+                if src.endswith(".cu"):
+                    ext.sources[i] = src  # keep name
         build_ext.build_extensions(self)
 
 ext_modules = [
@@ -28,7 +31,12 @@ ext_modules = [
         include_dirs=["."],
         extra_compile_args={
             "cxx": ["-O3", "-std=c++17"],
-            "nvcc": ["-O3", "-std=c++17", "--expt-relaxed-constexpr"]
+            "nvcc": [
+                "-O3",
+                "-std=c++17",
+                "--expt-relaxed-constexpr",
+                "-Xcompiler", "-fPIC"
+            ]
         },
         libraries=["cudart"],
     )
